2997cb2455588398c0caf9e3ecaf909f0734e54e
sundayefeh <topracer1988@gmail.com>
2019-10-27 11:24:58 +0100

    added readme

--- app/src/androidTest/java/com/example/acme/UITest.java
+++ app/src/androidTest/java/com/example/acme/UITest.java
@@ -12,9 +12,8 @@ import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 
 @RunWith(AndroidJUnit4.class)
 public class UITest {
