2895f18d012a06340c7cadbe125d58bd326730cb
sundayefeh <topracer1988@gmail.com>
2019-10-28 12:14:56 +0100

    change app id

--- app/src/androidTest/java/com/example/acme/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/acme/ExampleInstrumentedTest.java
@@ -23,7 +23,7 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.example.practice01", appContext.getPackageName());
+        assertEquals("com.example.acme", appContext.getPackageName());
     }
 
 
